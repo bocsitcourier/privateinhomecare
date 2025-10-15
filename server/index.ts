003b375f44066d43087d415669ef91d5753e82a4
Ayush Mehrotra <ayushmehrotra1212@gmail.com>
2025-10-15 17:25:03 +0530

    added

--- server/index.ts
+++ server/index.ts
@@ -248,7 +248,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     httpOnly: true,
     sameSite: 'lax',
     domain: '.privateinhomecaregiver.com',
